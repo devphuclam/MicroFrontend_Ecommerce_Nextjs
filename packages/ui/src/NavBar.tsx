e8612ccf68280d477ff79cf10115b9679b7af7cf
devphuclam <devphuclam@gmail.com>
2025-11-24 16:33:05 +0700

    More config

---
+++ packages/ui/src/NavBar.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import './NavBar.module.scss';
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>Hello NavBar</div>
+    </div>
+  );
+};
+
+export default NavBar;
