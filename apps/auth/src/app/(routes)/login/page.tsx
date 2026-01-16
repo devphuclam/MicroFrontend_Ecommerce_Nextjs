325f537371dd03e674b2f6d62f5a8251f7b4c3b4
devphuclam <devphuclam@gmail.com>
2026-01-16 20:07:42 +0700

    Finished +

--- apps/auth/src/app/(routes)/login/page.tsx
+++ apps/auth/src/app/(routes)/login/page.tsx
@@ -1,49 +1,154 @@
-'use client'
-import React from "react";
-import Image from "next/image";
+'use client';
+import React from 'react';
+import Image from 'next/image';
 import './login.scss';
-import { IllustrationImage } from "../../assets/images/index";
-import { Checkbox, Button } from "antd";
-import { GoogleIcon } from "@/app/assets/icons/GoogleIcon";
-import InputWithTagName from "@/app/components/InputWithTagName/InputWithTagName";
+import { IllustrationImage } from '../../assets/images/index';
+import { Checkbox, Button } from 'antd';
+import { GoogleIcon } from '@/app/assets/icons/GoogleIcon';
+import InputWithTagName from '@/app/components/InputWithTagName/InputWithTagName';
 import { FittoryLogo } from '@repo/ui/images/FittoryLogo';
 
 export default function LoginPage() {
-    return (
-        <div className="login-page-container">
-            <div className="form-section">
-                <div className="form-container">
-
-                    <div className="form-header-section">
-                        <FittoryLogo width={'auto'} height={150} />
-                        <div style={{ fontSize: "3rem", fontWeight: '500', lineHeight: "100%" }}>Welcome Back</div>
-                        <div style={{ fontSize: "1.5rem", fontWeight: '300', color: "#636364" }}>Welcome Back! Please enter your details.</div>
-                    </div>
-                    <div className="form-input-section">
-                        <InputWithTagName tagName="Email" variant="outlined" size="large" placeholder="Enter your Email" />
-                        <InputWithTagName tagName="Password" type="password" variant="outlined" size="large" placeholder="******" />
-                    </div>
-                    <div className="form-options-section">
-                        <Checkbox style={{ fontSize: "1rem", color: "#181818", lineHeight: "100%", fontWeight: "600" }}>Remember me</Checkbox>
-                        <Button variant="link" color="default" type="link" style={{ fontSize: "1rem", lineHeight: "100%", fontWeight: "600" }}>Forgot Password?</Button>
-                    </div>
-                    <div className="form-button-section">
-                        <Button type="primary" size="large" variant="solid" color="danger" danger style={{ width: "100%", padding: "1.5rem", fontSize: "1.2rem", borderRadius: "20px" }}>Sign in</Button>
-                        <Button type="default" icon={<GoogleIcon width={24} height={24} />} size="large" variant="outlined" color="default" style={{ width: "100%", padding: "1.5rem", fontSize: "1.2rem", borderRadius: "20px", fontWeight: "600" }}>Sign in with Google</Button>
-                        <div style={{ fontSize: "0.8rem", color: "#636364", fontWeight: "500", flexDirection: "row", display: "flex", gap: "1rem", justifyContent: "center", alignItems: "center" }}>Don't have an account? <Button variant="link" color="danger" type="link" style={{ width: "30%" }}>Sign up for free!</Button></div>
-                    </div>
-
-
-
-
-                </div>
-
+  return (
+    <div className='login-page-container'>
+      <div className='form-section'>
+        <div className='form-container'>
+          <div className='form-header-section'>
+            <FittoryLogo width={'auto'} height={150} />
+            <div
+              style={{
+                fontSize: '3rem',
+                fontWeight: '500',
+                lineHeight: '100%',
+              }}
+            >
+              Welcome Back
             </div>
-            <div className="illustration-section">
-                <IllustrationImage width={'w-full'} height={'99vh'} style={{ margin: '0' }} />
+            <div
+              style={{
+                fontSize: '1.5rem',
+                fontWeight: '300',
+                color: '#636364',
+              }}
+            >
+              Welcome Back! Please enter your details.
             </div>
-
-
+          </div>
+          <div className='form-input-section'>
+            <InputWithTagName
+              tagName='Email'
+              variant='outlined'
+              size='large'
+              placeholder='Enter your Email'
+            />
+            <InputWithTagName
+              tagName='Password'
+              type='password'
+              variant='outlined'
+              size='large'
+              placeholder='******'
+            />
+          </div>
+          <div className='form-options-section'>
+            <Checkbox
+              style={{
+                fontSize: '1rem',
+                color: '#181818',
+                lineHeight: '100%',
+                fontWeight: '600',
+              }}
+            >
+              Remember me
+            </Checkbox>
+            <Button
+              variant='link'
+              color='default'
+              type='link'
+              style={{
+                fontSize: '1rem',
+                lineHeight: '100%',
+                fontWeight: '600',
+              }}
+              onClick={() => {
+                window.location.assign('http://localhost:3003/forgot-password');
+              }}
+            >
+              Forgot Password?
+            </Button>
+          </div>
+          <div className='form-button-section'>
+            <Button
+              type='primary'
+              size='large'
+              variant='solid'
+              color='danger'
+              danger
+              style={{
+                width: '100%',
+                padding: '1.5rem',
+                fontSize: '1.2rem',
+                borderRadius: '20px',
+              }}
+              onClick={() => {
+                window.location.assign('http://localhost:3000');
+              }}
+            >
+              Sign in
+            </Button>
+            <Button
+              type='default'
+              icon={<GoogleIcon width={24} height={24} />}
+              size='large'
+              variant='outlined'
+              color='default'
+              style={{
+                width: '100%',
+                padding: '1.5rem',
+                fontSize: '1.2rem',
+                borderRadius: '20px',
+                fontWeight: '600',
+              }}
+              onClick={() => {
+                window.location.assign('http://localhost:3000');
+              }}
+            >
+              Sign in with Google
+            </Button>
+            <div
+              style={{
+                fontSize: '0.8rem',
+                color: '#636364',
+                fontWeight: '500',
+                flexDirection: 'row',
+                display: 'flex',
+                gap: '1rem',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              Don't have an account?{' '}
+              <Button
+                variant='link'
+                color='danger'
+                type='link'
+                style={{ width: '30%' }}
+                onClick={() => {
+                  window.location.assign('http://localhost:3003/sign-up');
+                }}
+              >
+                Sign up for free!
+              </Button>
+            </div>
+          </div>
         </div>
-    )
-} 
+      </div>
+      <div className='illustration-section'>
+        <IllustrationImage
+          width={'w-full'}
+          height={'99vh'}
+          style={{ margin: '0' }}
+        />
+      </div>
+    </div>
+  );
+}
